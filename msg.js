22b00eb482f1bde312904727abe14b2250976415
daijian1998 <44472925+daijian1998@users.noreply.github.com>
2020-08-20 10:24:18 +0800

    Add files via upload

--- msg.js
+++ msg.js
@@ -3,23 +3,13 @@ var timer = null
 Component({
 
   data: {
-    icon: '',
     tip: "this is a toptip",
     type:'warn',
     show: false,
-    isActive: ''
-
-  },
-  properties: {
-    Msgtype: {
-      type: String,
-      value: "info"
-    },
 
   },
   methods: {
     showTip(tip, type = "") {
-      console.log(tip+"--"+type)
       let that = this
       if (timer) { //如果当前消息还没有结束的话先结束消息并且清空定时器
         clearInterval(timer)
